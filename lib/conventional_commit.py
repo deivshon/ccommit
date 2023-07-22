72fa4da0e2235c2804c8352bb363553614b86067
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2023-07-22 21:40:12 +0200

    feat: :sparkles: run actual commit after getting all components

--- lib/conventional_commit.py
+++ lib/conventional_commit.py
@@ -1,7 +1,15 @@
+import sys
+import subprocess
+
 from lib.builder import ConventionalCommitBuilder
 
 
 def main():
     cc = ConventionalCommitBuilder()
     commitMessage = cc.interrogate()
-    print(commitMessage)
+    if commitMessage is None:
+        sys.exit(0)
+
+    subprocess.run(
+        ["git", "commit", "-m", commitMessage.short_message,
+            "-m", commitMessage.long_message])
